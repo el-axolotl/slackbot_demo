a8db70dbdfa951b6dfaa799955680300b2e4955b
el-axolotl <36735312+el-axolotl@users.noreply.github.com>
2026-08-05 10:51:37 -0600

    Moved provider block

--- infra/providers.tf
+++ infra/providers.tf
@@ -6,3 +6,15 @@ terraform {
     }
   }
 }
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      app         = "slackbot_demo",
+      environment = var.env
+    }
+  }
+
+}
