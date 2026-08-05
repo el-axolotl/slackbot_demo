ef5213dd5fd3d92e6ee114baff437050bc5c5414
el-axolotl <36735312+el-axolotl@users.noreply.github.com>
2026-08-05 12:28:55 -0600

    Refactor/infra code (#17)
    * Combined files
    
    * Moved provider block
    
    * Add tf backend
    
    * Fix backend
    
    * Add s3
    
    * Fix CODEOWNERS

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,26 +1,39 @@
-variable "name" {
-  description = "The name of the app."
-
-  type    = string
-  default = "slackbot_demo"
-}
-
-variable "region" {
-  description = "The AWS region to deploy all resources."
-
-  type    = string
-  default = "us-west-2"
-}
-
-variable "env" {
-  description = "The environment to deploy all resources."
-
-  type = string
+variable "bucket_force_destroy" {
+  description = "Whether to force destroy the S3 bucket even if it contains objects."
+  type        = bool
 }
 
 variable "cloudwatch_log_retention_in_days" {
   description = "The number of days to retain cloudwatch logs."
+  type        = string
+  default     = "1"
+}
 
-  type    = string
-  default = "1"
+variable "env" {
+  description = "The environment to deploy all resources."
+  type        = string
+}
+
+variable "lambda_runtime" {
+  description = "The runtime version to use for the lambda function."
+  type        = string
+  default     = "python3.12"
+}
+
+variable "name" {
+  description = "The name of the app."
+  type        = string
+  default     = "slackbot_demo"
+}
+
+variable "region" {
+  description = "The AWS region to deploy all resources."
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "repo" {
+  description = "The name of the repository."
+  type        = string
+  default     = "slackbot_demo"
 }
